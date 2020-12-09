6cfff9b4660d188f93996a622ac2a863ad2788e3
“Bogdan <mihaibogdanleparda@yahoo.com>
2020-12-09 20:53:13 +0200

    tema

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,31 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //1
 
-        }
+        int x;
+        x=2+5;
+        System.out.println(x); //2
+
+        float a,b,c;
+        a=12;
+        b=5;
+        c=a/b;
+        System.out.println(c); //3
+
+        int e,f,h;
+        e=-5+8*6;
+        System.out.println(e);
+
+        f= (55+9)%9;
+        System.out.println(f);
+
+        double  g;
+        g = 20.0 + ((-3.0 * 5.0) / 8.0);
+        System.out.println(g);
+
+        h= 5+((15/3)*2) - 8%3;
+        System.out.println(h); //4
+    }
 
 }
