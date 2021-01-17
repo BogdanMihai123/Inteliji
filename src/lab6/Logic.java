94bfd5fde4926f8c8d7caff8b62bb071e4973d63
“Bogdan <mihaibogdanleparda@yahoo.com>
2021-01-17 19:22:50 +0200

    lab

---
+++ src/lab6/Logic.java
@@ -0,0 +1,62 @@
+package lab6;
+
+import java.util.List;
+
+public class Logic {
+
+    public void printList(List<Integer> myList){
+        for (int i=0;i<myList.size(); i++){
+            System.out.println(myList.get(i));
+        }
+    } // list ex.1
+
+    public void addnumbertoList(List<Integer> myList, int number){
+        myList.add(number);
+        for (int i=0;i<myList.size();i++){
+            System.out.println(myList.get(i));
+        }
+    } // list ex.2
+
+    public void printListfromnumber(List<Integer> myList, int number){
+        for( int i=number;i<myList.size();i++){
+            System.out.println(myList.get(i));
+        }
+    } //list ex.3
+
+    public void printreverselist(List<Integer> myList){
+        for(int i=myList.size()-1;i>=0;i--){
+            System.out.println(myList.get(i));
+        }
+    } //list ex.4
+
+    public void addnumbertofirstposition(List<Integer> Mylist, int number){
+        Mylist.add(0,number);
+        Mylist.remove(1);
+        for (int i=0; i<Mylist.size();i++){
+            System.out.println(Mylist.get(i));
+        }
+    } // list ex.6
+
+    public void printdetailedlist(List<Integer> mylist){
+        for (int i=0; i<mylist.size();i++){
+            System.out.println("Pe pozitia" + " " + i + " "+"este valoarea" + " " + mylist.get(i));
+        }
+    } // list ex.7
+
+    public int biggestvaluefromlist(List<Integer> mylist){
+        int max=mylist.get(0);
+        for (int i=1;i<mylist.size();i++){
+            if (mylist.get(i)>max){
+                max=mylist.get(i);
+            }
+        }
+        return max;
+    } //list ex.8
+
+    public void stringList(List<String> myList, int number, String valoare){
+        myList.add(number,valoare);
+        for (int i=0; i<myList.size();i++){
+            System.out.println(myList.get(i));
+        }
+    } // list ex. 5
+}
---
+++ src/lab6/lab6.java
@@ -0,0 +1,38 @@
+package lab6;
+
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+public class lab6 {
+
+    public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<>();
+
+        myList.add(2);
+        myList.add(1);
+        myList.add(21);
+        myList.add(3);
+        myList.add(4);
+
+        Logic logic= new Logic();
+
+        //logic.printList(myList);
+        //logic.addnumbertoList(myList,22);
+        //logic.printListfromnumber(myList,3);
+        // logic.printreverselist(myList);
+        //logic.addnumbertofirstposition(myList,100);
+        //logic.printdetailedlist(myList);
+        //int max;
+        //max= logic.biggestvaluefromlist(myList);
+        //System.out.println(max);
+
+        List<String> Mylist= new ArrayList<>();
+
+        Mylist.add("YES");
+        Mylist.add("NO");
+        Mylist.add("Maybe");
+        logic.stringList(Mylist,2,"NEVER");
+    }
+}
